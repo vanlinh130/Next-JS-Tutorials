ec1f71b2145f535904e87589a640684a8e0a80be
vanlinh1330 <levanlinh1330@gamil.com>
2023-03-17 01:27:21 +0700

    Bai 33: SSR with thông so dong

---
+++ next-pre-rendering/pages/news/[category].js
@@ -0,0 +1,37 @@
+function ArticleListByCategory({articles, category}) {
+    return ( 
+        <>
+            <h1>Showing new for category <i>{category}</i></h1>
+            {
+                articles.map(article => {
+                    return (
+                        <div key={article.id}>
+                            <h2>
+                                {article.id} {article.title}
+                            </h2>
+                            <p>{article.description}</p>
+                            <hr/>
+                        </div>
+                    )
+                })
+            }
+        </>
+     );
+}
+
+export default ArticleListByCategory;
+
+export async function getServerSideProps(context) {
+    const { params } = context
+    const { category } = params
+    const response = await fetch(`http://localhost:4000/news?category=${category}`)
+
+    const data = await response.json()
+
+    return {
+        props: {
+            articles: data,
+            category,
+        }
+    }
+}
