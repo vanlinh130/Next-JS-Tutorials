a89658168fdcf50ed98e2a90b1c209a59eb56ce1
vanlinh1330 <levanlinh1330@gamil.com>
2023-03-18 00:09:40 +0700

    Bài 34: getServerSideProps Context

--- next-pre-rendering/pages/news/[category].js
+++ next-pre-rendering/pages/news/[category].js
@@ -22,7 +22,10 @@ function ArticleListByCategory({articles, category}) {
 export default ArticleListByCategory;
 
 export async function getServerSideProps(context) {
-    const { params } = context
+    const { params, req, res, query } = context
+    console.log(query);
+    console.log(req.headers.cookie);
+    res.setHeader('Set-Cookie', ['name=Vishwas'])
     const { category } = params
     const response = await fetch(`http://localhost:4000/news?category=${category}`)
 
