77dcaa598af28bebd0394c3645a79b5631be2558
vanlinh1330 <levanlinh1330@gamil.com>
2023-03-20 10:01:49 +0700

    Bai 43: API POST Request

--- next-api/pages/comments/index.js
+++ next-api/pages/comments/index.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function CommentsPage() {
     const [comments, setComments] = useState([])
+    const [comment, setComment] = useState('')
 
     const fetchComments = async () => {
         const response = await fetch('/api/comments')
@@ -9,8 +10,20 @@ function CommentsPage() {
         setComments(data)
     }
 
+    const submitComment = async () => {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ comment }),
+            headers: { 'Content-Type': 'application/json'}
+        })
+        const data = await response.json()
+        console.log(data);
+    }
+
     return ( 
         <>
+            <input type="text" value={comment} onChange={(e) => setComment(e.target.value)} />
+            <button onClick={submitComment}>Submit Comments</button>
             <button onClick={fetchComments}>Load Comments</button>
             {comments.map((comment) => {
                 return (
