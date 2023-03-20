3570aacf297e03187dec75a702897d50f98204fb
vanlinh1330 <levanlinh1330@gamil.com>
2023-03-20 10:41:47 +0700

    Bai 45: API DELETE Request

--- next-api/pages/comments/index.js
+++ next-api/pages/comments/index.js
@@ -20,6 +20,15 @@ function CommentsPage() {
         console.log(data);
     }
 
+    const deleteComment = async (commentId) => {    
+        const response = await fetch(`/api/comments/${commentId}`, {
+            method: 'DELETE',
+        })
+        const data = await response.json()
+        console.log(data);
+        fetchComments()
+    }
+
     return ( 
         <>
             <input type="text" value={comment} onChange={(e) => setComment(e.target.value)} />
@@ -29,6 +38,7 @@ function CommentsPage() {
                 return (
                     <div key={comment.id}>
                         {comment.id} {comment.text}
+                        <button onClick={() => deleteComment(comment.id)}>Delete</button>
                     </div>
                 )
             })}
