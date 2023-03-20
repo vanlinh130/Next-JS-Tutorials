6c2ec6af50e287c715222299be2cb1440d1f6123
vanlinh1330 <levanlinh1330@gamil.com>
2023-03-21 00:57:49 +0700

    Bai 64: Bien moi truong

--- next-misc/pages/blog/[blogId].js
+++ next-misc/pages/blog/[blogId].js
@@ -7,7 +7,9 @@ function Blog({ title, description }) {
                 <title>deƯ{title}</title>   
                 <meta name="description" content={description}/>
             </Head> 
-            <h1 className="content">Article</h1>
+            <h1 className="content">
+                Env Analytics  {process.env.NEXT_PUBLIC_ANALYTICS_ID}
+            </h1>
         </>
 
      );
@@ -16,6 +18,11 @@ function Blog({ title, description }) {
 export default Blog;
 
 export async function getStaticPaths() {
+    const user = process.env.DB_USER
+    const password = process.env.DB_PASSWORD
+
+    console.log(`Connecting to database with username ${user} and password ${password}`);
+
     return {
         paths: [{ params: { blogId: '1'}}],
         fallback: false
