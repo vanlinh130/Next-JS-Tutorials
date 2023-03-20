9fbd82fbbfd799744d325242345b28c30d881328
vanlinh1330 <levanlinh1330@gamil.com>
2023-03-20 17:14:57 +0700

    Bai 60: Static HTM Export

--- next-misc/pages/pets.js
+++ next-misc/pages/pets.js
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import img from '../public/1.jpg'
+// import Image from 'next/image'
+// import img from '../public/1.jpg'
 
 function PetsPage() {
     return ( 
         <div>
-            <Image src={img} placeholder='blur' alt="pet" width='280' height='420'  />
+            {/* <Image src={img} placeholder='blur' alt="pet" width='280' height='420'  />
 
             {['1', '2', '3', '4', '5'].map((path) => {
                 return (
@@ -12,7 +12,8 @@ function PetsPage() {
                         <Image src={`/${path}.jpg`} alt="pet" width='280' height='420'  />
                     </div>
                 )
-            })}
+            })} */}
+            pets
         </div>
      );
 }
