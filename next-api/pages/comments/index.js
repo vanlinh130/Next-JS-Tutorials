568de6f268a5a2acf515df6bb25265bc134953ae
vanlinh1330 <levanlinh1330@gamil.com>
2023-03-20 09:40:54 +0700

    Bai 42: API GET Request

---
+++ next-api/pages/comments/index.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function CommentsPage() {
+    const [comments, setComments] = useState([])
+
+    const fetchComments = async () => {
+        const response = await fetch('/api/comments')
+        const data = await response.json()
+        setComments(data)
+    }
+
+    return ( 
+        <>
+            <button onClick={fetchComments}>Load Comments</button>
+            {comments.map((comment) => {
+                return (
+                    <div key={comment.id}>
+                        {comment.id} {comment.text}
+                    </div>
+                )
+            })}
+        </>
+     );
+}
+
+export default CommentsPage;
