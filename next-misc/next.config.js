16058a2206d6628c62fb24e743c157c893996911
vanlinh1330 <levanlinh1330@gamil.com>
2023-03-21 00:13:49 +0700

    Bai 63: Chuyen Huong

--- next-misc/next.config.js
+++ next-misc/next.config.js
@@ -4,3 +4,22 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+
+module.exports = {
+  reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+        permanent: false,
+      },
+      {
+        source: '/old-blog/:id',
+        destination: '/new-blog/:id',
+        permanent: true,
+      }
+    ]
+  }
+}
