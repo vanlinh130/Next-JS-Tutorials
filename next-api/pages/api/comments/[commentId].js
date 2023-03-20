f0d2971a5c57b70fbfd792a5325ba6d6fcd22d9e
vanlinh1330 <levanlinh1330@gamil.com>
2023-03-20 11:45:41 +0700

    Bai 45: APIs and Pre-rendering

--- next-api/pages/api/comments/[commentId].js
+++ next-api/pages/api/comments/[commentId].js
@@ -1,23 +1,59 @@
 import { comments } from '../../../data/comments';
 
-export default function handler(req, res) {
-    const { commentId } = req.query
 
-    if (req.method === 'GET') {
-        const comment = comments.find(
-            (comment) => comment.id === parseInt(commentId))
-        res.status(200).json(comment)
-    } else if (req.method === 'DELETE') {
-        const deleteComment = comments.find(
-            (comment) => comment.id === parseInt(commentId)
-        )
+// export default function handler(req, res) {
+//     const { commentId } = req.query
 
-        const index = comments.findIndex(
-            (comment) => comment.id === parseInt(commentId)
-        )
+//     if (req.method === 'GET') {
+//         const comment = comments.find(
+//             (comment) => comment.id === parseInt(commentId))
+//         res.status(200).json(comment)
+//     } else if (req.method === 'DELETE') {
+//         const deleteComment = comments.find(
+//             (comment) => comment.id === parseInt(commentId)
+//         )
 
-        comments.splice(index, 1)
+//         const index = comments.findIndex(
+//             (comment) => comment.id === parseInt(commentId)
+//         )
 
-        res.status(200).json(deleteComment)
+//         comments.splice(index, 1)
+
+//         res.status(200).json(deleteComment)
+//     }
+// }    
+
+function Comment({ comment }) {
+    return ( 
+        <div>
+            {comment.id} {comment.text}
+        </div>
+     );
+}
+
+export default Comment;
+
+export async function getStaticPaths() {
+    return {
+        paths : [
+            {params: { commentId: 1}},
+            {params: { commentId: 2}},
+            {params: { commentId: 3}},
+        ],
+        fallback: false
+    }
+}
+
+export async function getStaticProps(context) {
+    const { params } = context;
+    const { commentId} = params
+
+    const comment = comments.find((comment) => comment.id ===  parseInt(commentId))
+    console.log(comment);
+
+    return {
+        props: {
+            comment,
+        },
     }
 }
