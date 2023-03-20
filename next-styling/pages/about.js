dc6ddb092c57ed26820e7eb3d6485467bf351b4e
vanlinh1330 <levanlinh1330@gamil.com>
2023-03-20 13:25:42 +0700

    Bai 50: Global Styles

---
+++ next-styling/pages/about.js
@@ -0,0 +1,10 @@
+function About() {
+    return ( 
+       <>
+            <h2>About Page</h2>
+            <button className="btn btn-primary">Primary</button>
+       </>
+     );
+}
+
+export default About;
---
+++ next-styling/pages/contact.js
@@ -0,0 +1,7 @@
+function Contact() {
+    return ( 
+        <h2>Contact Page</h2>
+     );
+}
+
+export default Contact;
