567d4580ac909f6dd4c62034167a4b931a72b9ae
vanlinh1330 <levanlinh1330@gamil.com>
2023-03-21 00:03:37 +0700

    Bai 62: preview Mode

--- next-misc/pages/_app.js
+++ next-misc/pages/_app.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
-import '@/styles/globals.css'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import '../styles/globals.css'
 import '../styles/layout.css'
 
 export default function App({ Component, pageProps }) {
--- next-misc/pages/about.js
+++ next-misc/pages/about.js
@@ -1,4 +1,4 @@
-import Footer from "@/components/Footer";
+import Footer from "../components/Footer";
 import Head from "next/head";
 
 function About() {
