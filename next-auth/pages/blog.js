bdf1c5abd5ece3ffa66c8e421bba07a30f8373d8
vanlinh1330 <levanlinh1330@gamil.com>
2023-03-21 11:55:14 +0700

    Bai 74: Securing Pages Server-side

--- next-auth/pages/blog.js
+++ next-auth/pages/blog.js
@@ -13,6 +13,15 @@ export default Blog;
 export async function getServerSideProps(context) {
     const session = await getSession(context)
 
+    if (!session) {
+        return {
+          redirect: {
+            destination: '/api/auth/signin?callbackUrl=http://localhost:3000/blog',
+            permanent: false
+          }
+        }
+      }
+
     return {
         props: {
             session,
