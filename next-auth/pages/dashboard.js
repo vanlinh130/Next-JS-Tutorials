28bdf41f06dc4495eec040037c0a98853cb6a021
vanlinh1330 <levanlinh1330@gamil.com>
2023-03-21 10:47:36 +0700

    Bai 71: Securing Pages Client-side

--- next-auth/pages/dashboard.js
+++ next-auth/pages/dashboard.js
@@ -1,4 +1,25 @@
+import { getSession, signIn } from "next-auth/react";
+import { useEffect, useState } from "react";
+
 function Dashboard() {
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const securePage = async () => {
+            const session = await getSession()
+            if (!session) {
+                signIn()
+            } else (
+                setLoading(false)
+            )
+        }
+        securePage()
+    }, [])
+
+    if (loading) {
+        return <h2>Loading...</h2>
+    }
+
     return ( 
         <h1>Dashboard</h1>
      );
